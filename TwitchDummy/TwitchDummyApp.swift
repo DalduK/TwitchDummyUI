41220cf7d00010b5cb13de2f2396d231ae49e3a4
DalduK <przemyslaw7wozny@gmail.com>
2021-10-19 11:43:18 +0200

    Initial Commit

---
+++ TwitchDummy/TwitchDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchDummyApp.swift
+//  TwitchDummy
+//
+//  Created by Przemysław Woźny on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitchDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
